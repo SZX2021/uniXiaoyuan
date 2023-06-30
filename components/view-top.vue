e819227f7d31966f4ad26fb88bcdaa251c09737e
哆啦B梦 <1149268899@qq.com>
2023-06-30 15:49:58 +0800

    增加了TO_TOP

---
+++ components/view-top.vue
@@ -0,0 +1,66 @@
+<template>
+  <view>
+    <view class="top" v-if="topShow" @click="goTop()">
+      <uni-icons type="navigate-filled" :size="55" color="#2b85e4" />
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "viewTop",
+    data() {
+      return {
+        topShow: false,
+      };
+    },
+    props: {
+      scrollTop: {
+        type: Number,
+        default: 0
+      },
+    },
+
+    methods: {
+      topData(e) {
+        //这个e直接是滚动时候的高度
+        this.topShow = e > 390
+      },
+      goTop() {
+        uni.pageScrollTo({
+          scrollTop: this.scrollTop,
+          duration: 300
+        });
+      },
+    }
+  }
+</script>
+
+<style lang="scss">
+  .top {
+    height: 70rpx;
+    width: 70rpx;
+    text-align: center;
+    line-height: 70rpx;
+    border-radius: 50%;
+    background-color: rgba($color: #FFFFFF, $alpha: 0.8);
+    border: 1rpx solid #ddd;
+    position: fixed;
+    z-index: 999;
+    // 距离右边
+    right: 25rpx;
+    // 距离底部距离
+    bottom: 150rpx;
+    animation: heart 0.3s;
+
+    @keyframes heart {
+      0% {
+        right: -30rpx;
+      }
+
+      100% {
+        right: 30rpx;
+      }
+    }
+  }
+</style>
